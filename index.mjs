0d65500dca2aad9318dde759920dd4e8d138ea2e
Weichun <weichun@csie.tw>
2025-02-08 12:10:33 +0800

    also pull month report due to delay

--- index.mjs
+++ index.mjs
@@ -3,7 +3,8 @@ import axios from 'axios';
 import * as https from 'https';
 
 const pullingSources = [
-    'https://service.cbs.tw/public/upload/files/json/{year}_earthquakeew.json',
+    'https://cbs.tw/public/upload/files/json/{year}_earthquakeew.json',
+    'https://cbs.tw/public/upload/files/json/{year}{month}.json',
 ];
 
 const LAMBDA_ARN = process.env.LAMBDA_ARN;
@@ -37,8 +38,20 @@ function filterMessage(oMessage) {
     return message;
 }
 
+function getAlarmPageKey(am) {
+    let pageKey = '';
+
+    if(am.page_key.length < 12) {
+        pageKey += (am.release_time.replace(/\D/g, '').substr(2, 4));
+    }
+
+    pageKey += am.page_key;
+
+    return pageKey;
+}
+
 function formatAlarm(am) {
-    let url = 'https://cbs.tw/' + (am.release_time.replace(/\D/g, '').substr(2, 4)) + am.page_key;
+    let url = 'https://cbs.tw/' + getAlarmPageKey(am);
 
     let message = filterMessage(am.CMAMtext);
     message += '\n---\n';
@@ -54,7 +67,10 @@ function isTestMessage(message) {
 function getHttpsClient() {
     if(_httpsClient === null) {
         _httpsClient = axios.create({
-            httpsAgent: new https.Agent({keepAlive: true}),
+            httpsAgent: new https.Agent({
+                keepAlive: true,
+                keepAliveMsecs: 50 * 1000,
+            }),
             headers: {
                 'accept': 'application/json',
             },
@@ -74,31 +90,52 @@ export const handler = async (event) => {
     // console.log(JSON.stringify(event));
     const httpsClient = getHttpsClient();
     const lambdaClient = getLambdaClient();
+    const NOW = new Date();
+    const MONTH_REPORT_TYPE = Symbol("MONTH_REPORT");
 
-    let today = new Date().toLocaleString('zh-TW', {
+    let today = NOW.toLocaleString('zh-TW', {
         year: 'numeric',
         month: '2-digit',
         day: '2-digit',
         timeZone: 'Asia/Taipei',
     });
     let year = today.substr(0, 4);
+    let month = today.substr(5, 2);
     let sourceType = [];
     for(let i = 0, j = pullingSources.length; i < j; i++) {
-        let s = pullingSources[i] = pullingSources[i].replace('{year}', year);
-        let sourceMatch = s.match(/\/\d+_(\w+)\.json/);
-        if(sourceMatch.length >= 2) {
+        let s = pullingSources[i] = pullingSources[i]
+            .replace('{year}', year)
+            .replace('{month}', month);
+        // s += '?t=' + NOW.getTime();
+        let sourceMatch;
+        if((sourceMatch = s.match(/\/\d{4}_(\w+)\.json/)) && sourceMatch.length === 2) {
             sourceType[i] = sourceMatch[1];
+        } else if ((sourceMatch = s.match(/\/\d{6}\.json/)) && sourceMatch.length === 1) {
+            sourceType[i] = MONTH_REPORT_TYPE;
         }
     }
 
+    let textTypes = new Set(sourceType.filter(t => typeof t === 'string'));
+
     let promises = [];
+    let timestamp = NOW.getTime();
     for(let i = 0, j = pullingSources.length; i < j; i++) {
-        let src = pullingSources[i];
-        promises.push(axios.get(src).then(r => {
+        let src = new URL(pullingSources[i]);
+        let type = sourceType[i];
+
+        src.searchParams.append('t', timestamp);
+        promises.push(axios.get(src.toString()).then(r => {
             if(r.headers.get('content-type') === 'application/json' && r.data.success === true) {
-                return r.data.data.alertMessages.sort((a, b) => {
-                    return a.release_time.localeCompare(b.release_time)
-                });
+                if(typeof type === 'string') {                    
+                    return r.data.data.alertMessages.sort((a, b) => {
+                        return a.release_time.localeCompare(b.release_time)
+                    });
+                } else if (type === MONTH_REPORT_TYPE) {
+                    let flattenData = Object.values(r.data.data).flatMap(Object.values).flatMap(Object.values);
+                    return flattenData.sort((a, b) => {
+                        return a.release_time.localeCompare(b.release_time)
+                    });
+                }
             }
             return null;
         }).catch(console.error));
@@ -111,17 +148,32 @@ export const handler = async (event) => {
 
     for(let i = 0, j = pullingSources.length; i < j; i++) {
         let result = results[i];
-        let type = sourceType[i] || '';
+        let type = sourceType[i];
         if(!result) continue;
-        let key = TYPE_PREFIX + type;
-        let latestTime = latestTimeMap[key] || '';
+        if (typeof type === 'string') {
+            let key = TYPE_PREFIX + type;
+            let latestTime = latestTimeMap[key] || '';
 
-        result.forEach(r => {
-            if(latestTime.localeCompare(r.release_time) < 0 && !isTestMessage(r.CMAMtext)) {
-                newAlarms.push(r);
-                tagUpdate[key] = r.release_time;
-            }
-        });
+            result.forEach(r => {
+                if(latestTime.localeCompare(r.release_time) < 0 && !isTestMessage(r.CMAMtext)) {
+                    let pageKey = getAlarmPageKey(r);
+                    newAlarms[pageKey] = r;
+                    tagUpdate[key] = r.release_time;
+                }
+            });
+        } else if (type === MONTH_REPORT_TYPE) {
+            result.forEach(r => {
+                if(!textTypes.has(r.alertType)) return;
+                let type = r.alertType;
+                let key = TYPE_PREFIX + type;
+                let latestTime = latestTimeMap[key] || '';
+                if(latestTime.localeCompare(r.release_time) < 0 && !isTestMessage(r.CMAMtext)) {
+                    let pageKey = getAlarmPageKey(r);
+                    newAlarms[pageKey] = r;
+                    tagUpdate[key] = r.release_time;
+                }
+            });
+        }
     }
 
     let postPromises = [];
@@ -137,7 +189,7 @@ export const handler = async (event) => {
     // post to channel
     if(process.env.SLACK_WEBHOOKS) {        
         let webhooks = process.env.SLACK_WEBHOOKS.split(',');
-        newAlarms.forEach(am => {
+        Object.values(newAlarms).forEach(am => {
             let formattedMessage = formatAlarm(am);
             webhooks.forEach(webhook => {                
                 postPromises.push(httpsClient.post(
